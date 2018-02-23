ce0d00e0cd8b214bd26500f5f7df0608d50b27c1
Orlando Andaur <oaandaurc@falabella.cl>
2018-02-23 00:13:35 -0300

    create example

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+const Example = ({ number }) => <div>{number}</div>
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data.map((number, index) => <Example number={number} key={index} /> )}
       </div>
     );
   }
